f832897e534a94f1be5eae4b7a1709d7fa344890
Amit Dubey <amit.dubey@tothenew.com>
2017-07-25 17:01:44 +0530

    added endpoint to get todos

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -20,6 +19,19 @@ app.post('/todos', (req, res) => {
     res.status(400).send(err);
   });
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+      res.send({
+        todos,
+      })
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  )
+});
+
 app.listen(3000, () => {
   console.log(`Started on port 3000`)
 })
